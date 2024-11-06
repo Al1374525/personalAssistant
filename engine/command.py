4a76863f8f1103cdde7c414eae11492086d609fb
Al1374525 <albertayiteyadjin@gmail.com>
2024-11-05 19:34:14 -0500

    voice assistant is working

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 
 def speak(text):
     
@@ -9,4 +10,23 @@ def speak(text):
     print(voices)
     engine.say(text)
     engine.runAndWait()
-speak("I love Ghana")
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening.... ")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen(source, 10, 6)
+    try:
+        print("recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+    except Exception as e:
+        return ""
+    return query.lower()
+
+text = takeCommand()
+    
+speak(text)
