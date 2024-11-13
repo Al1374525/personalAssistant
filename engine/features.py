ebd7693f64bea47a062e43b3a5f62eb8577079b6
Al1374525 <albertayiteyadjin@gmail.com>
2024-11-12 20:43:31 -0500

    opening different apps work

--- engine/features.py
+++ engine/features.py
@@ -1,10 +1,15 @@
 import re
+import sqlite3
 from playsound import playsound
 import eel
 from engine.command import speak
 from engine.config import ASSISTANT_NAME
 import os
 import pywhatkit as kit
+import webbrowser
+
+conn = sqlite3.connect("delta.db")
+cursor = conn.cursor()
 
 #Playing assistant sound function
 
@@ -19,11 +24,50 @@ def openCommand(query):
     query = query.replace("open", "")
     query.lower()
     
-    if query !="":
-        speak("Opening "+ query)
-        os.system("start " + query)
-    else:
-        speak("not found")
+    # Strip query to get the app name as a single string
+    app_name = " ".join(query.split())
+
+    if app_name:
+        try:
+            # Query to check if there is a matching path in sys_command
+            cursor.execute(
+                'SELECT path FROM sys_command WHERE name = ?', (app_name,))
+            results = cursor.fetchall()
+
+            if results:
+                speak("Opening " + query)
+                os.startfile(results[0][0])  # Use the path from the first result
+
+            else:
+                # Check for URL in web_command table if no path found
+                cursor.execute(
+                    'SELECT url FROM web_command WHERE name = ?', (app_name,))
+                results = cursor.fetchall()
+                
+                if results:
+                    speak("Opening " + query)
+                    webbrowser.open(results[0][0])  # Use the URL from the first result
+                else:
+                    # If no matching command or URL, attempt to start as a system command
+                    speak("Opening " + query)
+                    try:
+                        os.system('start ' + query)
+                    except FileNotFoundError:
+                        speak("File not found")
+                    except Exception as e:
+                        speak(f"An error occurred: {str(e)}")
+        except Exception as e:
+            speak(f"Something went wrong: {str(e)}")
+    
+    
+    # if query !="":
+    #     speak("Opening "+ query)
+    #     os.system("start " + query)
+    # else:
+    #     speak("not found")
+        
+        
+        
 
 def PlayYoutube(query):
     search_term = extract_yt_term(query)
